e7ac7c9dc9da18524908e73ffbfd56a77c8a8d7d
murali <muralimittireddy1234@gmail.com>
2025-08-18 21:57:36 +0530

    testing cd pipeline

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -7,4 +7,4 @@ api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(expenses.router, prefix="/expenses", tags=["expenses"])
 api_router.include_router(budgets.router, prefix="/budgets", tags=["budgets"])
-api_router.include_router(analytics.router, prefix="/analytics", tags=["analytics"]) # Include analytics router1
+api_router.include_router(analytics.router, prefix="/analytics", tags=["analytics"]) # Include analytics router s
